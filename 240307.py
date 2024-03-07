a0cc7da6cca9b94c007918010914a4e98fda151e
dangerousdogcookie <162543257+jimin0318@users.noreply.github.com>
2024-03-07 15:50:09 +0900

    Update 240307.py

--- 240307.py
+++ 240307.py
@@ -8,3 +8,17 @@ b=20
 print("a의 값은",a)
 print("b의 값은",b)
 print("a와 b의 합은",a+b)
+
+print("30을 8로 나눈 몫은", 30//8)
+print("30을 8로 나눈 나머지는", 30%8)
+print("30을 8로 나눈 결과는", 30/8)
+
+a = '시은 우진 화이팅!!'
+print(a)
+print()
+print(a)
+
+
+name=input("이름이 무엇인가요? ")
+age=int(input('몇 살인가요? '))
+print(name,"님은 내년에는", age+1, "살이 됩니다.")
